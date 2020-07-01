653aaabebf478b9591af31d3a3db17a959a66480
matias-selzer <matias.selzer@gmail.com>
2020-07-01 18:39:37 -0300

    FPS counter

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public Text fpsLabel;
+    public float timeInterval;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ShowFPS", 0, timeInterval);
+    }
+
+
+    void ShowFPS()
+    {
+        int FPS = (int)(1f / Time.unscaledDeltaTime);
+        fpsLabel.text = "FPS = " + FPS;
+    }
+}
