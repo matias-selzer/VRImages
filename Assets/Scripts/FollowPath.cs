43745e0bc7f2c526e0968684d8bbd21a4150cb87
matias-selzer <matias.selzer@gmail.com>
2020-07-08 20:18:53 -0300

    Anda a velocidad constante

--- Assets/Scripts/FollowPath.cs
+++ Assets/Scripts/FollowPath.cs
@@ -6,25 +6,34 @@ public class FollowPath : MonoBehaviour
 {
     public Transform[] positions;
     private int index;
-    public float moveSpeed, rotateSpeed;
+    public float moveTime,moveDistance, rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        InvokeRepeating("Move", 0, moveTime);
+    }
+
+    private void Move()
+    {
+        Vector3 normalizedDirection = Vector3.Normalize(positions[index].position - transform.position);
+        transform.position = transform.position + normalizedDirection * moveDistance;
+
+        if (Vector3.Distance(transform.position, positions[index].position) < 0.1f)
+        {
+            index = (index + 1) % positions.Length;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, positions[index].position, moveSpeed * Time.deltaTime);
-        if(Vector3.Distance(transform.position, positions[index].position) < 0.1f)
-        {
-            index = (index + 1) % positions.Length;
-        }
+        //transform.position = Vector3.MoveTowards(transform.position, positions[index].position, moveSpeed * Time.deltaTime);
+        
+        
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, positions[index].rotation, rotateSpeed * Time.deltaTime);
-        
     }
 }
