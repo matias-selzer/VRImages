284bfd7e8408a6a4b84456e54a5ab3acfcabe6f4
matias-selzer <matias.selzer@gmail.com>
2020-07-12 16:39:01 -0300

    Tests

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -16,7 +16,7 @@ public class FPSCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     private void Update()
