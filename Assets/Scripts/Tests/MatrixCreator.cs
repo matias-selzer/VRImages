c51a7369e02edbda334e52971f59f81f78e1a0ac
matias-selzer <matias.selzer@gmail.com>
2020-09-04 16:08:45 -0300

    Some test scene for an overview image for the paper

---
+++ Assets/Scripts/Tests/MatrixCreator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixCreator : MonoBehaviour
+{
+    public GameObject cube,sphere;
+    public float cantx,canty,cantz;
+    public Texture[] textures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMatrix();
+        CreateSpheresMatrix();
+    }
+
+    void CreateSpheresMatrix()
+    {
+        for (int i = 0; i < cantx; i++)
+        {
+            for (int j = 0; j < canty; j++)
+            {
+                for (int k = 0; k < cantz; k++)
+                {
+                    Vector3 newPos = new Vector3(i, j, k);
+                    GameObject newSphere = Instantiate(sphere, newPos, Quaternion.Euler(0,Random.Range(0,360),0));
+                    //newSphere.transform.localScale *= 0.7f;
+                    newSphere.GetComponent<Renderer>().material.mainTexture = textures[Random.Range(0, textures.Length)];
+                }
+            }
+        }
+    }
+
+    void CreateMatrix()
+    {
+        for(int i = 0; i < cantx; i++)
+        {
+            for(int j = 0; j < canty; j++)
+            {
+                for(int k = 0; k < cantz; k++)
+                {
+                    Vector3 newPos = new Vector3(i, j, k);
+                    GameObject newCube = Instantiate(cube, newPos, transform.rotation);
+                }
+            }
+        }
+    }
+}
